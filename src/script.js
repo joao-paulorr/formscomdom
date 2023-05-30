85a57417b2924e43edb1ed91db85482bf4c43935
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-05-30 15:33:15 +0000

    x

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 function alerta() {
     const user = document.getElementsByTagName("input")[0].value
     const senha = document.getElementsByTagName("input")[1].value
+    alert(`meu usúario é ${user} e a senha é ${senha}`)
 }
