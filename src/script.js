0822dd4e35506cf6602c148b68209b7559038738
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-05-30 18:18:02 +0000

    3

--- src/script.js
+++ src/script.js
@@ -14,4 +14,6 @@ function alerta() {
 function quadrado() {
     let quadrado = document.getElementById("quadrado")
     quadrado.style.backgroundColor = prompt("Qual é a cor? digite em inglês")
+    quadrado.style.width = prompt("Qual a largura? acima de 100")+"px"
+    quadrado.style.height = prompt("Qual o tamanho? acima de 100")+"px"
 }
