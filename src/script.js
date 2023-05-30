1f846eb82ac73bd2ed158f498af0e0620a1bff0f
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-05-30 15:47:30 +0000

    a

--- src/script.js
+++ src/script.js
@@ -1,5 +1,12 @@
 function alerta() {
     const user = document.getElementsByTagName("input")[0].value
     const senha = document.getElementsByTagName("input")[1].value
-    alert(`meu usúario é ${user} e a senha é ${senha}`)
+
+
+    if (user == "joaopaulorr" && senha == "umdoistres45") {
+        alert("Bem vindo, João!")
+
+    } else {
+        alert("usuário invalido. vai tomar no seu cu")
+    }
 }
