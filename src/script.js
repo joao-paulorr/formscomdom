cdc04bdfe91ed4c99139f42f6a24da3d31907abf
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-06-02 11:11:19 +0000

    d

--- src/script.js
+++ src/script.js
@@ -1,15 +1,4 @@
 function alerta() {
-    const user = document.getElementsByTagName("input")[0].value
-    const senha = document.getElementsByTagName("input")[1].value
-
-
-    if (user == "joaopaulorr" && senha == "umdoistres45" || user == "soph" && senha == "123456") {
-    } else {
-        alert("usuário invalido. vai tomar no seu cu")
-    }
-}
-
-function login() {
     const user = document.getElementById("user").value;
     const senha = document.getElementById("senha").value;
 
